fb3f8cf9de7f8e6054d7c7bfabd324c92b99a786
Thomas-Ln <thomas-ln@laposte.net>
2020-12-23 15:28:58 +0100

    3_ChunkyMonkey

---
+++ scrimba_js_christmas_calendar_event_2020/3_ChunkyMonkey.js
@@ -0,0 +1,26 @@
+function chunkyMonkey(values, size) {
+    //  write code here.
+    return [values.slice(0, size), values.slice(size, values.length)];
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('chunkyMonkey()', () => {
+    it('returns largest positive integer possible for digit count', () => {
+        // arrange
+        const values = ["a", "b", "c", "d"];
+        const size = 2;
+        
+        // act
+        const result = chunkyMonkey(values, size);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([["a", "b"], ["c", "d"]]);
+    });
+});
